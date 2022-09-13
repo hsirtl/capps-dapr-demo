c88f0b06b5c091fa33302353501ccc5a087a7795
hsirtl <hsirtl@microsoft.com>
2022-09-13 12:05:18 +0200

    App Insights extended

--- MyFrontEnd/Pages/Index.cshtml.cs
+++ MyFrontEnd/Pages/Index.cshtml.cs
@@ -16,8 +16,11 @@ public class IndexModel : PageModel
 
     public async Task OnGet()
     {
-        _logger.LogInformation("Index page visited at {DT}",
-            DateTime.UtcNow.ToLongTimeString());
+        _logger.LogDebug("Debug: Index page visited at {DT}", DateTime.UtcNow.ToLongTimeString());
+        _logger.LogInformation("Information: Index page visited at {DT}", DateTime.UtcNow.ToLongTimeString());
+        _logger.LogWarning("Warning: Index page visited at {DT}", DateTime.UtcNow.ToLongTimeString());
+        _logger.LogError("Error: Index page visited at {DT}", DateTime.UtcNow.ToLongTimeString());
+        _logger.LogCritical("Critical: Index page visited at {DT}", DateTime.UtcNow.ToLongTimeString());
 
         var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
             HttpMethod.Get,
--- MyFrontEnd/Program.cs
+++ MyFrontEnd/Program.cs
@@ -1,5 +1,3 @@
-using Google.Api;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
